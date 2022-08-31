39fa788a8cb42a57b5e04346c469fe461482339d
Alejandro <alejandro199898@gmail.com>
2022-08-30 21:45:05 -0700

    add total cart price

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,7 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -14,14 +16,27 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
         <p className="cart__title">Cart</p>
       </div>
       <div className="cart__body">
-        <ul className="cart__items">
-          {/* Check if cart is empty */}
-          {Array.isArray(cartItems) && !cartItems.length ? (
-            <p>No Items In Your Cart.</p>
-          ) : (
-            cartItems.map((item) => <CartItem key={item.id} {...item} />)
-          )}
-        </ul>
+        {/* Check if cart is empty */}
+        {Array.isArray(cartItems) && !cartItems.length ? (
+          <p>No Items In Your Cart.</p>
+        ) : (
+          <div>
+            <ul className="cart__items">
+              {cartItems.map((item) => (
+                <CartItem key={item.id} {...item} />
+              ))}
+            </ul>
+            <div>
+              Cart Total:{" "}
+              {formatCurrency(
+                cartItems.reduce((total, cartItem) => {
+                  const item = storeItems.find((i) => i.id === cartItem.id);
+                  return total + (item?.price || 0) * cartItem.quantity;
+                }, 0)
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
