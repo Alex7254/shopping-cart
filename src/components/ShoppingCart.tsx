9cef14721cb258ee17edcd5a64f2d413ac8c714f
Alejandro <alejandro199898@gmail.com>
2022-08-30 19:08:15 -0700

    create shopping cart component

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,3 +1,28 @@
-export function ShoppingCart () {
-  return <h1>shoppong</h1>
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { cartItems } = useShoppingCart();
+
+  return (
+    <div className="cart">
+      <div className="cart__header">
+        <p className="cart__title">Cart</p>
+      </div>
+      <div className="cart__body">
+        <ul className="cart__items">
+          {/* Check if cart is empty */}
+          {Array.isArray(cartItems) && !cartItems.length ? (
+            <p>No Items In Your Cart.</p>
+          ) : (
+            cartItems.map((item) => <CartItem key={item.id} {...item} />)
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
